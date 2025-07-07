14805dd887c86334b3017b3b09badc711dfc68b0
botaneren <botaneren@hotmail.com>
2025-07-07 22:23:43 +0300

    Revert "t7"

--- src/AnotherClass.java
+++ src/AnotherClass.java
@@ -1,9 +1,5 @@
 public class AnotherClass {
 
-    //Techpro branch, version t7
-    public String string1 = "Biri sen, biri ben, iki damla yas akti gozlerimden";
-}
-=======
   //Remote version r1
   public String string1 = "Bir ömür yetmezki";
   
--- src/MyClass.java
+++ src/MyClass.java
@@ -1,4 +1,4 @@
-public class qqMyClass {
+public class MyClass {
     //versiyon1
 
     public String string1 = "Bu bir stringdir";
